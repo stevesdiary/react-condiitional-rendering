da92666456e9d1e154e559ac4314aa575f5c320e
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2022-04-08 14:39:20 +0100

    Used javascript &&'ampersand' Operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,18 @@
 import React from "react";
+import Login from "./login";
+//Using another way of expression
 
-var isLoggedIn = true;
-function renderConditionally(){
-  if (isLoggedIn === true){
-    return <h1>Hello</h1>
-  } else {
-    return <form className="form">
-    <input type="text" placeholder="Username" />
-    <input type="password" placeholder="Password" />
-    <button type="submit">Login</button>
-  </form>
-  }
-}
+const currentTime = new Date(2022, 4, 1, 13).getHours();
+console.log(currentTime)
 
+var isLoggedIn = false;
 function App() {
   return (
-    <div className="container">
-      {renderConditionally()} 
-
+    <div className="container"> 
+    {/* { isLoggedIn ? <h1>Hello</h1> : <Login /> } */}
+    {currentTime >12 && <h1>Why are you still working at this time?</h1>}
     </div>
+    //[What this condition means is that: if the currentTime is greater than 12 then the second expression will not be rendered.
   );
 }
-
 export default App;
