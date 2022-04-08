8efa8404a92a088dc5245d539c78a6dc31062891
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2022-04-08 13:30:34 +0100

    Used react tenary operator for the conditional statement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
+var isLoggedIn = true;
+function renderConditionally(){
+  if (isLoggedIn === true){
+    return <h1>Hello</h1>
+  } else {
+    return <form className="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+  }
+}
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {renderConditionally()} 
+
     </div>
   );
 }
